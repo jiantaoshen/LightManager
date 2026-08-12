d4915a1852dc833ddabf3af26c1fa83cdff34340
Jiantao <144024751+jiantaoshen@users.noreply.github.com>
2026-08-12 09:27:50 +0200

    Add Microsoft SQL migrationn

--- LightManager.Server/Migrations/20260812072121_InitialSqlServer.Designer.cs
+++ LightManager.Server/Migrations/20260812072121_InitialSqlServer.Designer.cs
@@ -4,6 +4,7 @@ using LightManager.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LightManager.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260812072121_InitialSqlServer")]
+    partial class InitialSqlServer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
