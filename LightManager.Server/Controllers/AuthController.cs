8439aacf95bfb15f6e5ac597c5df33af41bbee86
Jiantao <144024751+jiantaoshen@users.noreply.github.com>
2026-08-13 09:28:26 +0200

    Remove Consonle.WriteLine

--- LightManager.Server/Controllers/AuthController.cs
+++ LightManager.Server/Controllers/AuthController.cs
@@ -25,8 +25,6 @@ public class AuthController : ControllerBase
     [HttpPost("register")]
     public async Task<IActionResult> Register(RegisterDTO dto)
     {
-        Console.WriteLine("REGISTER endpoint called");
-
         var user = new ApplicationUser
         {
             UserName = dto.FullName,
@@ -49,8 +47,6 @@ public class AuthController : ControllerBase
     [HttpPost("login")]
     public async Task<IActionResult> Login(LoginDTO dto)
     {
-        Console.WriteLine("Login endpoint called");
-
         var user = await _userManager.FindByEmailAsync(dto.Email);
 
         if (user == null)
