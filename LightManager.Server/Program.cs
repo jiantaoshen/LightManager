0d7a98c739d3b014716c5c3ca27db6cd117414cf
Jiantao <144024751+jiantaoshen@users.noreply.github.com>
2026-08-13 10:26:49 +0200

    Test SQL Server

--- LightManager.Server/Program.cs
+++ LightManager.Server/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>options.UseNpgsql(connectionString));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>options.UseSqlServer(connectionString));
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>() // Add roles support
